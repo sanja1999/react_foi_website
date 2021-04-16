1e657082f4e6cc4cb49b6114d09ab9fbab0e8ef1
sanja1999 <sajfar.sanja@gmail.com>
2021-04-16 21:13:46 +0200

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            <div className="Footer-Text">
+                <p>Copyright &copy; 2021. S. Šajfar</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.scss';
+
+
+
+const Main = (props) => {
+    return (
+        <main>
+            {props.children}
+        </main>
+    );
+}
+export default Main;
